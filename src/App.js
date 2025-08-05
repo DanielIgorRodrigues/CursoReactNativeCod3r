b54594b4014405404dccc651918fd476bc8c88de
Daniel Igor Rodrigues <daniel.igor.rodrigues@gmail.com>
2025-08-04 23:15:02 -0300

    inclusão dos primeiros exercícios

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, StatusBar, Platform, StyleSheet} from 'react-native';
+
+import Aleatorio from './components/Aleatorio';
+// import MinMax from './components/MinMax';
+// import CompoPadrao, { Comp1, Comp2 } from './components/Multi';
+// import Primeiro from './components/Primeiro';
+
+
+
+export default () => (
+    <View style={style.App}>
+
+        <Aleatorio min={5} max={100}/>
+        {/* <MinMax min={3} max={20}/>
+        <MinMax min={3} max={20}/>
+        <CompoPadrao />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro />  */}
+    </View>
+);
+
+const style = StyleSheet.create({
+    App: {
+        flex: 1, 
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight: 0,
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+    }
+})
---
+++ src/components/estilo.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    fontGrande: {
+        fontSize: 32,
+        textAlign: "center"
+    },
+
+    fontMedia: {
+        fontSize: 24
+    },
+
+    fontPequena: {
+        fontSize: 16
+    }
+})
